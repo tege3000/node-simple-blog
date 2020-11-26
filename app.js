3f28a5db00d48b891f5707368ef6b53e91289f22
Tito <teetoegeonu@gmail.com>
2020-11-26 15:51:46 +0100

    ADD - learning about server side rendering using the ejs view engine

--- app.js
+++ app.js
@@ -3,18 +3,27 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+
 // listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-    //res.send("<p>Home Page</p>");
-    res.sendFile("./views/index.html", { root: __dirname });
+    const blogs = [
+        {title: "Yoshi finds eggs", snippet: "Lorem ipsum dolar sit amet consectetur"},
+        {title: "Mario finds stars", snippet: "Lorem ipsum dolar sit amet consectetur"},
+        {title: "How to defeat bowser", snippet: "Lorem ipsum dolar sit amet consectetur"},
+    ];
+    res.render("index", {title: "Home", blogs});
 });
 
 app.get("/about", (req, res) => {
-    //res.send("<p>About Page</p>");
-    res.sendFile("./views/about.html", { root: __dirname });
+    res.render("about", {title: "About"});
+});
 
+app.get("/blogs/create", (req, res) => {
+    res.render("create", {title: "Create new Blog"});
 });
 
 // redirects
@@ -24,5 +33,5 @@ app.get("/about-us", (req, res) => {
 
 // 404 Page
 app.use((req, res) => {
-    res.status(404).sendFile("./views/404.html", { root: __dirname });
+    res.status(404).render("404", {title: "404"});
 });
