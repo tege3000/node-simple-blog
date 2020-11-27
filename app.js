a9e5a61f8998478326978d6b8c626e89cbe76aaa
Tito <teetoegeonu@gmail.com>
2020-11-27 17:17:01 +0100

    ADD - connected application to mongodb, and created blog Schema, and mongoose model

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI = "mongodb+srv://enisen:abc1234@node-tuts.pgsiu.mongodb.net/node-tuts?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => {
+        console.log("connected to db");
+
+        // only listen for requests after database connecting is set up
+        app.listen(3000);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
 // register view engine
 app.set("view engine", "ejs");
 
-// listen for requests
-app.listen(3000);
+
 
 //middleware & static files
 app.use(express.static("public"));
