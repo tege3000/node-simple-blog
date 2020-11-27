d72e70ce46167792a40dbc5bba2b027fbab71762
Tito <teetoegeonu@gmail.com>
2020-11-27 17:31:36 +0100

    ADD - Learned how to save documents inside the mongodb collection

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-
+const Blog = require("./models/blog");
 // express app
 const app = express();
-
 // connect to mongodb
 const dbURI = "mongodb+srv://enisen:abc1234@node-tuts.pgsiu.mongodb.net/node-tuts?retryWrites=true&w=majority";
+
 
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then((result) => {
@@ -19,15 +19,31 @@ mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
         console.log(err);
     });
 
-// register view engine
+// register view engin 
 app.set("view engine", "ejs");
-
-
 
 //middleware & static files
 app.use(express.static("public"));
 app.use(morgan("dev"));
 
+// mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+    const blog  = new Blog({
+        title: "New Blog",
+        snippet: "About my new blog",
+        body: "More about my new blog"
+    });
+
+    blog.save()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
+
+//routes
 app.get("/", (req, res) => {
     const blogs = [
         {title: "Yoshi finds eggs", snippet: "Lorem ipsum dolar sit amet consectetur"},
