07288c04ec7d5d085fd13dbbf771721d2c2b1f6b
Tito <teetoegeonu@gmail.com>
2020-12-03 17:33:50 +0100

    ADD - learned about POST and GET requests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const Blog = require("./models/blog");
+const { render } = require("ejs");
 // express app
 const app = express();
 // connect to mongodb
@@ -24,6 +25,7 @@ app.set("view engine", "ejs");
 
 //middleware & static files
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); // for accepting form data
 app.use(morgan("dev"));
 
 
@@ -45,6 +47,28 @@ app.get("/blogs", (req, res) => {
         .catch((err) => {
             console.log(err);
         });
+});
+
+app.post("/blogs", (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save() 
+        .then((result) => {
+            res.redirect("/blogs");
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
+
+app.get("/blogs/:id", (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then((result) => {
+            res.render("details", {blog: result, title: "Blog Details"});
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 });
 
 app.get("/blogs/create", (req, res) => {
